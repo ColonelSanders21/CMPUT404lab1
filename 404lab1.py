ecbae8217544479492565b5c8d8d6b06d6cbd33c
Anders Johnson <anders.johnson.email@gmail.com>
2020-01-07 10:42:46 -0500

    Removed venv folder

--- 404lab1.py
+++ 404lab1.py
@@ -2,5 +2,8 @@ import requests
 
 print(requests.__version__)
 
-google = requests.get('https://www.google.com/')
-print(google)
+# google = requests.get('https://www.google.com/')
+# print(google)
+
+raw = requests.get('https://raw.githubusercontent.com/ColonelSanders21/CMPUT404lab1/master/404lab1.py')
+print(raw.content)
